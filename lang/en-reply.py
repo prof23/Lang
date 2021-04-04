e8988390a19e97c9ad93ca2958aa2d1a995a9236
prof23 <81886358+prof23@users.noreply.github.com>
2021-04-04 03:07:29 +0300

    Add files via upload

---
+++ lang/en-reply.py
@@ -0,0 +1,522 @@
+doneadd = """Done add the group ([{}])
+and promote admins and creator"""
+
+doneadd2 = "Done add the group [{}]"
+
+doneadded = "Group [{}] already added"
+
+disabled = "Group [{}] already removed"
+
+disabl = "Done removed group [{}]\nthe data's of this group will deleted in 24h"
+
+
+userNocc = "Sorry I can't get the (user/id)"
+
+locked = "<b>By :</b> {} <b>{} already lock</b>" 
+lock = "<b>By :</b> {} <b>locked {} in group</b>" 
+
+unlocked = "<b>By :</b> {} <b>{} already unlock</b>" 
+unlock = "<b>By :</b> {} <b>unlock {} in group</b>"
+kickbotadd = "Done kick the bot <a href='http://t.me/{}'>{}</a>"
+
+ADDed = "<b>By :</b> {} <b>{} already enable</b>" 
+ADD = "<b>By :</b> {} <b>enable {} in group</b>" 
+
+unADDed = "<b>By :</b> {} <b>{} already disabled</b>" 
+unADD = "<b>By :</b> {} <b>disable {} in group</b>"
+
+true = "✅"
+
+false = "❎"
+
+settings = "<b>Group settings - ({})</b>\n<b>✅ - unlocked | ❎ - locked</b>"
+st2 = "<b>Group settings - ({})\n✅ - enable | ❎ - disable\nFlood time : ({})\nFlood msgs : ({})</b>"
+
+
+
+notforyou = "Sorry it's not for you"
+
+Corder = "Cancel"
+
+doneCO = "Canceled"
+ARdoneCO = "It has been already canceled"
+setRK = "{} <b>promote {}</b>"
+remRK = "{} <b>Done demote from {}</b>"
+
+DsetRK = "{} <b>already {}</b>"
+DremRK = "{} <b>not {}</b>"
+
+ban = "*ban* {} *from group*"
+Dban = "{} <b>already ban</b>"
+
+banall = "<b>banall</b> {} <b>from groups</b>"
+Dbanall = "{} <b>already banall</b>"
+
+unban = "*unban* {} *from group*"
+Dunban = "{} <b>already unban</b>"
+
+unbanall = "<b>unbanall</b> {} <b>from groups</b>"
+Dunbanall = "{} <b>already unbanall</b>"
+
+restricted = "*mute* {} *in group*"
+Drestricted = "{} <b>already muted</b>"
+
+unrestricted = "*unmute* {} *in group*"
+Dunrestricted = "{} <b>already unmuted</b>"
+
+restrictedall = "<b>muteall </b> {} <bin groups</b>"
+Drestrictedall = "{} <b>already mutedall </b>"
+
+unrestrictedall = "<b>unmuteall </b> {} <b>in groups</b>"
+Dunrestrictedall = "{} <b>already unmutedall</b>"
+
+haveRank = "can't kick mute ,{}"
+haveKick = "Sorry, User kicked or left"
+delList = "delete {}"
+
+TKflood = "<b>Mute </b>{}<b> msgs {} time {}</b>"
+
+DoneSet = "<b>Done set {} ({})</b>"
+
+
+haveRc = "{} already muted"
+
+showlist = "{} list :\n{}"
+listempty = "{} list epmty"
+creatorempty = "Creator epmty"
+listempty2 = "<b>The list is empty</b>"
+Doneblock = "<b>Block ({}) in group ({})</b>"
+Adoneblock = "<b>({}) it's block in group</b>"
+
+
+StB = "<b>Block the sticker in ({})</b>"
+StA = "<b>The sticker is already block in ({})</b>"
+
+GfB = "<b>Block the Gif in ({})</b>"
+GfA = "<b>The Gif is already block in ({})</b>"
+
+PhB = "<b>Block the photo in ({})</b>"
+PhA = "<b>The photo is already block in ({})</b>"
+
+
+unDoneblock = "<b>unblock ({}) in ({})</b>"
+unAdoneblock = "<b>({}) it's unblock in group</b>"
+
+unStB = "<b>unBlock the sticker in ({})</b>"
+unStA = "<b>The sticker is already unblock in ({})</b>"
+
+unGfB = "<b>unBlock the Gif in ({})</b>"
+unGfA = "<b>The Gif is already unblock in ({})</b>"
+
+unPhB = "<b>unBlock the photo in ({})</b>"
+unPhA = "<b>The photo is already unblock in ({})</b>"
+
+blocklist = "<b>{} list in {}</b>"
+banlist = "<b>All list in the Bot</b>"
+
+
+blocklist2 = "blocks"
+replylist = "replays"
+replylistBot = "replay bot"
+BlocklistRm = "Clear blocks words"
+replylistRm = "Clear words replay"
+STreplylistRm = "Clear sticker replay"
+VOreplylistRm = "Clear voice replay"
+GFreplylistRm = "Clear gifs replay"
+
+BlocklistEm = "List is epmty"
+replylistEm = "List is epmty"
+STreplylistEm = "List is epmty"
+GFreplylistEm = "List is epmty"
+VOreplylistEm = "List is epmty"
+
+Blocklistone = "unblock"
+Delall = "You can clear the list by clicking"
+Delall2 = "unlock all"
+DoneUNblock = "unblocked"
+
+DelallR = "You can clear reply list by clicking"
+Delall2R= "Clear all"
+
+Donewel = "<b>Change welcome to : <b>({})</b>"
+
+Sendreply = "Now send the reply for %s\n<b>{fn}</b> : first namen<b>{us}</b> : useranme\n<b>{id}</b> : ID\n<b>{rk}</b> : Rank\n<b>{cn}</b> : group title\nyou can use html"
+
+SRtext = "<b>Done save the reply {}</b>"
+SRst = "<b>Done save the sticker {}</b>"
+SRgf = "<b>Done save the Gif {}</b>"
+SRvo = "<b>Done save the voice {}</b>"
+
+Drp = "Done deleted reply <b>({})</b>"
+Norp = "there is no reply <b>({})</b>"
+Yrp = "there is reply <b>({})</b>"
+allbandel = "Clear blocked all"
+
+alltkdel = "Clear muted all"
+
+allbanE = "List is empty"
+
+alltkE = "List is empty"
+
+Ddelbanall = "Blocked list deleted"
+
+Ddeltkall = "Muted list deleted"
+
+Dsetname = "<b>Done change group name {}</b>"
+Dsetabout = "<b>done change group about {}</b>"
+
+Dsetphoto = "<b>Done change group photo</b>"
+Dsetlk = "Done change group link {}"
+yourmsgs = "<b>Msgs : {}</b>"
+youredits = "<b>Edits : {}</b>"
+
+youraddcontact = "<b>you're contacts  : {} \n done deleted you're contacts </b>"
+
+yes = "Yes"
+no = "No"
+kickme = "<b>are you sure?</b>"
+Dkickme ="kicked With your will"
+Nkickme = "Canceled"
+
+Rsudo = "BasicSudo"
+Rsudos = "Sudo"
+Rcreator = "Creator"
+Rowner = "owner"
+Radmin = "Amdin"
+Rvip = "VIP"
+Rmember = "member"
+
+Rrank = "Rank : {}"
+Rmsgs = "Msgs : ({})"
+Rrate = "Rate : ({})"
+Redits = "Edits : ({})"
+RID = "ID : {}"
+RIDPH = "Name : {}\n ID : {}"
+RIDPHs = "Show you'r info"
+DPROadmins = "Done promote the admins in the bot"
+DsetSudosShow = "<b>Sudo order changed</b>"
+DsetSudosShowE = "Sorry there is a problem in html"
+
+sendto = "<b>Where you want to send?</b>"
+
+Dpinmsg = "<b>The message is pin</b>"
+Dunpinmsg = "<b>The message is unpin</b>"
+
+showstats = "<b>Bot statistics :\nGroups : ({})\nPV : ({})</b>"
+
+showGPlk = "<b>group link :</b>\n{}"
+
+PRsendtoGP = "<b>I will send this msg to groups</b>"
+
+PRsendtoPR = "<b>I will send this msg to PM</b>"
+
+DsendtoGP = "Done sent to groups\nsent to : <b>({})</b>\nnot sent : <b>({})</b>"
+DsendtoPR = "Done sent to PV\nsent to : <b>({})</b>\nnot sent  : <b>({})</b>"
+
+
+clickTOpv = "Go to PM"
+
+Toolong = "Sorry the list is long to send in one msg, press the button to view the list in PM"
+
+DoneDelList = "The list has been cleared"
+SetAuto = "Done change autoadd number to : ({})"
+
+NoMember = "Sorry the member is not in the group"
+
+cTsudo = "You can't (banall/muteall) sudo"
+
+Toolow = "Sorry the group is less then ({})"
+
+GiveMEall = "Sorry I can't activate the group. Please give me all the permissions"
+
+DoneleaveChat = "Done i leave from the group"
+
+
+banclick = "{} you are ban from useing the button's for 2m "
+
+
+NoDeleted = "Sorry there are no deleted accounts in the group"
+LenDeleted = "there {} deleted accounts in the group i will kick them"
+
+AdminO = """• admins orders :
+
+• ban <username | reply | id>
+• mute <username | reply | id>
+• bans - mutes
+
+• setphoto <reply to photo>
+• setlink <group link>
+• setabout <about>
+• setname <name>
+• settings
+• restrict settings"""
+
+OwnerO = """• Owner orders :
+
+• set - rem admin <username | reply | id>
+• set - rem vip <username | reply | id>
+• rem all <useranem | reply | id>
+• admins - vips
+
+• unblock - block <reply|words>
+• add - rem reply <words>
+• replys - blocks
+
+• setwelcome <words>
+• flood (msgs - time) <number>
+• flood set
+
+• setid <words>
+• delid
+
+• enable - disable <welcome | flood | replys | replysbot | kickme | id | ph id | bt id>"""
+
+CreatorO = """• Creator orders :
+
+• set - rem owner <username | reply | id>
+• enable - disable <pin>
+
+• kick <deleted|bots>
+• change lang"""
+
+SudosO = """• sudo orders :
+
+• add - rem group
+• set - rem creator <username | reply | id>
+• add - rem replybot <words>
+• replysbot"""
+
+SudoO = """• BasicSudo orders :
+
+• set - rem sudo <username | reply | id>
+• ban - unban all <username | reply | id>
+• mute - unmute all <username | reply | id>
+• alllist - sudos
+
+• send - fwd <reply>
+• leave <groups id>
+• groups - groups links
+
+• setsudosorder <words>
+• enable - disable <autoadd>
+• autoadd <number>
+
+• update < lang | bot >
+• rem - add > files
+• files"""
+
+Showall = """• Welcome to <a href=\"t.me/z8888\">LiL .</a>
+• You can use the following commands 
+-- -- -- -- --"""
+
+LockO = """• Lock and unlock orders:
+
+• lock - unlock
+• links
+• bots
+• video
+• note
+• music
+• voice
+• gifs
+• photo
+• inline
+• markdown
+• contact
+• files
+• sticker
+• fwd
+• username
+• english
+• arabic
+• tag
+• nf
+• edits
+• longtext
+
+• PS : use settings its easy"""
+
+lockC = "lock's orders"
+AdminC = "Admins orders"
+OwnerC = "Owner orders"
+CreatorC = "Creator orders"
+SudosC = "Sudo orders"
+SudoC = "BasicSudo orders"
+
+
+ShowGPN = "{}- Name : ({})\ID : (`{}`)"
+
+AsPh = '''• {}
+• {}
+• {}'''
+
+
+
+SudoOnle = "Sorry it's for BasicSudo"
+Chlang = "Choose the language - اختر اللغه"
+Dulang = "Done update bor lang"
+
+lang ={"ar":"🇵🇸 AR-Classic","en":"🇺🇸EN","arem":"🇵🇸 AR-Modern"}
+
+
+
+IDnPT = '''ID : {id}
+Username : {us}
+Rank : {rk}
+Msgs : {msgs}
+Edits : {edits}
+Rate : {rate}'''
+
+
+DsetIDShow = "<b>Doen change ID text</b>"
+Ddelid = "Done delete ID text"
+
+
+NoBots = "Sorry there are no bots in the group"
+LenBots = "there {} bots in the group i will kick them"
+
+Wres = "The bot will restart please wait"
+
+MoreInfo = "More info?"
+
+CKgps = "Check groups"
+
+
+Dckg = "\nDone check the groups and remove {}"
+
+
+ckpr = "Checking groups may take a little time"
+
+true2 = "✔️"
+false2 = "✖️"
+
+settingsRes = "<b>Group restrict settings </b>\n<b>✔️ unlocked - ✖️ locked</b>"
+
+pk = "⏪"
+fr = "⏩"
+
+hide = "Hide"
+
+sors = "• bot [{}] - [LiL .](t.me/Z8888)"
+
+dellink = "Deactivate link for Telegram"
+
+botstart = """Hello : {},
+{}, protection for groups you can add me by <a href=\"http://t.me/{}?startgroup=botstart\">click here</a>"""
+
+
+botstart = """• Welcome to {} protection bot for groups
+• you can add me by <a href=\"http://t.me/{}?startgroup=botstart\">click here</a>
+⎯ ⎯ ⎯ ⎯"""
+
+Tban = "ban"
+Tres = "mute"
+
+fset = "flood set : {}"
+Tfset = "You can change flood set to (ban |mute)"
+
+NOaddfiles = "Sorry there are no files from <a href=\"http://t.me/z8888\">(LiL .)</a>"
+
+addFiles = """Here are the files from <a href=\"http://t.me/z8888\">(LiL .)</a>
+you can download file by clicking"""
+
+
+Dua = """Done (download/update) ({})
+you can enable the file by sending files"""
+
+Files = """Here are the files from Bot
+you can enable or disable the file by clicking"""
+
+NOaddfiles2 = "Sorry there ar no files in bot you can add files by sending add files"
+
+dlFiles = """Here are the files from Bot
+you can rem the file by clicking"""
+
+
+Delfile = "Done rem file ({})"
+Delfiles = "Done remove all files"
+
+remall = """{} done rem from ({})"""
+
+remallN = """{} have no rank's"""
+
+
+RKgp = "groups"
+RKgpl = "groups links"
+RKaf = "add file"
+RKrf = "rem file"
+RKf = "files"
+RKub = "update bot"
+RKul = "update lang"
+sudostart = """Welcome , BasicSudo
+you can use the orders down blow 🔽"""
+
+
+Lenbans= "there {} bans in the group i will unban them"
+NobansC = "Sorry there are no bans in the group"
+
+Lenrks= "there {} mutes in the group i will unmute them"
+NorksC = "Sorry there are no mutes in the group"
+
+addor = " ({}) add to ?"
+Daddor = "add ({}) to ({})"
+SRph = "Done save the Photo ({})"
+SRfi = "Done save the File ({})"
+
+checkmem = "Sorry {} You must confirm that you are not a robot\nPress Yes to complete verification\nPress No to finish and fire you"
+checkmem2 = "Sorry {} You must confirm that you are not a robot\nChoose the correct answer to solve the equation\n{}"
+certain = "Certain"
+notcertain = "Not certain"
+ucertain = "Are you sure?"
+bancheck = "The member {} was banned from group\nbecause he/she didn't verify"
+
+age = {"older_than":"older than","newer_than":"newer than","aprox":"~"}
+
+tset = "twostep mode : {}"
+Ttset = "You can change the two-step mode to (two-step | equations)"
+Teq = "equations"
+Ttwo = "two-step"
+
+ReIDnPT = """ID : {ReID}
+Username : {Reus}
+Rank : {Rerank}
+Msgs : ({Remsgs})
+Edits : ({Reedits})
+Rate : ({Rerate})
+Date created : {Rage}"""
+
+IDnPT = '''ID : {id}
+Username : {us}
+Rank : {rk}
+Msgs : {msgs}
+Edits : {edits}
+Rate : {rate}
+Date created : {age}'''
+
+Rage = "Date created : {}"
+
+OwnerO = """• Owner orders :-
+
+• set - rem admin <username | reply | id>
+• set - rem gpadmin <username | reply | id>
+• set - rem vip <username | reply | id>
+• rem all <useranem | reply | id>
+• admins - vips
+
+• unblock - block <reply|words>
+• add - rem reply <words>
+• replys - blocks
+
+• setwelcome <words>
+• flood (msgs - time) <number>
+• flood set
+• twostep set
+
+• setid <words>
+• clear <number>
+• delid
+
+• enable - disable <twostep | welcome | flood | replys | replysbot | kickme | id | ph id | bt id>"""
+
+prGPadmin = """• Member : {} 
+• promote to admin in group"""
